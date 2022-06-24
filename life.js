d212e566070f8e1e17001c58169aff41e2014ff9
Nweneary Uzochukwu Winnie <99335288+Whiney96@users.noreply.github.com>
2022-06-24 07:43:07 -0700

    remove \n

--- life.js
+++ life.js
@@ -1,5 +1,5 @@
-var myName = "Winnie\n";
+var myName = "Winnie";
 var height = '5"9ft';
-var country = "Nigeria\n";
+var country = "Nigeria";
 
 document.write("Hello, my name is "+myName+".<br>I am "+height+" tall.<br>And I am from "+country);
