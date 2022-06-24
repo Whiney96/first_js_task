f82472bebf45000a896455c6e84e7200cd9dc32a
Nweneary Uzochukwu Winnie <99335288+Whiney96@users.noreply.github.com>
2022-06-24 07:42:22 -0700

    change \n to <br>

--- life.js
+++ life.js
@@ -2,4 +2,4 @@ var myName = "Winnie\n";
 var height = '5"9ft';
 var country = "Nigeria\n";
 
-document.write("Hello, my name is "+myName+".\n","I am "+height+" tall.\n","And I am from "+country);
+document.write("Hello, my name is "+myName+".<br>I am "+height+" tall.<br>And I am from "+country);
